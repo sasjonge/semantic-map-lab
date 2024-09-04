f7047aa7808964714788a5c2dedc73439fde2cc1
Sascha Jongebloed <sasjonge@uni-bremen.de>
2024-09-04 17:15:29 +0200

    added default setting file

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Launch ros web applications
 source ${ROS_WS}/devel/setup.bash
-roscore &
-roslaunch --wait rvizweb rvizweb.launch &
 
 # Start MongoDB and save data on working directory
 MONGODB_URL=mongodb://127.0.0.1:27017
@@ -14,6 +12,5 @@ jupyter lab workspaces import ${PWD}/binder/jupyterlab.jupyterlab-workspace
 
 # Launch Knowrob
 export KNOWROB_MONGODB_URI=${MONGODB_URL}/?appname=knowrob
-roslaunch --wait knowrob knowrob.launch &
 
 exec "$@"
