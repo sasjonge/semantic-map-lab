4ac1892ba6987b1388db0e83e7a69d7219ed6775
Sascha Jongebloed <sasjonge@uni-bremen.de>
2024-09-05 16:35:20 +0200

    updated reasoner and notebooks

--- reasoner/dfl_reasoner.py
+++ reasoner/dfl_reasoner.py
@@ -2,24 +2,24 @@ from knowrob import *
 
 import dfl.dlquery as dl
 
-def _iriOrVariable(x):
+def _iriOrVariable(reasoner, x):
     if x.isVariable():
         return x
-    return IRIAtom(str(x))
+    return IRIAtom(reasoner.expandName(str(x)))
 
-class SOMADFLReasoner(GoalReasoner):
+class SOMADFLReasoner(GoalDrivenReasoner):
     def __init__(self):
         super().__init__()
         self.reasoner = dl.DFLReasoner()
-        self.hasDisposition = IRIAtom("http://ease-crc.org/ont/SOMA_DFL.owl#hasDisposition")
-        self.isDispositionOf = IRIAtom("http://ease-crc.org/ont/SOMA_DFL.owl#isDispositionOf")
-        self.hasPart = IRIAtom("http://ease-crc.org/ont/SOMA_DFL.owl#hasPart")
-        self.isPartOf = IRIAtom("http://ease-crc.org/ont/SOMA_DFL.owl#isPartOf")
-        self.hasConstituent = IRIAtom("http://ease-crc.org/ont/SOMA_DFL.owl#hasConstituent")
-        self.isConstituentOf = IRIAtom("http://ease-crc.org/ont/SOMA_DFL.owl#isConstituentOf")
-        self.useMatch = IRIAtom("http://ease-crc.org/ont/SOMA_DFL.owl#useMatch")
-        self.isInstanceOf = IRIAtom("http://ease-crc.org/ont/SOMA_DFL.owl#isInstanceOf")
-        self.isSubclassOf = IRIAtom("http://ease-crc.org/ont/SOMA_DFL.owl#isSubclassOf")
+        self.hasDisposition = IRIAtom("http://www.ease-crc.org/ont/SOMA_DFL.owl#hasDisposition")
+        self.isDispositionOf = IRIAtom("http://www.ease-crc.org/ont/SOMA_DFL.owl#isDispositionOf")
+        self.hasPart = IRIAtom("http://www.ease-crc.org/ont/SOMA_DFL.owl#hasPart")
+        self.isPartOf = IRIAtom("http://www.ease-crc.org/ont/SOMA_DFL.owl#isPartOf")
+        self.hasConstituent = IRIAtom("http://www.ease-crc.org/ont/SOMA_DFL.owl#hasConstituent")
+        self.isConstituentOf = IRIAtom("http://www.ease-crc.org/ont/SOMA_DFL.owl#isConstituentOf")
+        self.useMatch = IRIAtom("http://www.ease-crc.org/ont/SOMA_DFL.owl#useMatch")
+        self.isInstanceOf = IRIAtom("http://www.ease-crc.org/ont/SOMA_DFL.owl#isInstanceOf")
+        self.isSubclassOf = IRIAtom("http://www.ease-crc.org/ont/SOMA_DFL.owl#isSubclassOf")
         self.rdfType = IRIAtom("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
         self.defineRelation(self.hasDisposition)
         self.defineRelation(self.hasPart)
@@ -32,22 +32,23 @@ class SOMADFLReasoner(GoalReasoner):
         self.defineRelation(self.isSubclassOf)
         self.simpleGoals = {self.hasPart, self.hasConstituent, self.hasDisposition, self.isDispositionOf, self.isPartOf, self.isConstituentOf, self.isInstanceOf, self.isSubclassOf}
         self.inverseProperties = {self.isDispositionOf: self.hasDisposition, self.isPartOf: self.hasPart, self.isConstituentOf: self.hasConstituent}
-        self.fnMap = {self.hasDisposition: lambda p, s, o, bounding: self._evaluateSimpleGoal(p, s, o, bounding, self.reasoner.whatDispositionsDoesObjectHave, self.reasoner.whatHasDisposition),
-                      self.hasPart: lambda p, s, o, bounding: self._evaluateSimpleGoal(p, s, o, bounding, self.reasoner.whatPartTypesDoesObjectHave, self.reasoner.whatHasPartType),
-                      self.hasConstituent: lambda p, s, o, bounding: self._evaluateSimpleGoal(p, s, o, bounding, self.reasoner.whatConstituentsDoesObjectHave, self.reasoner.whatHasConstituent),
-                      self.isSubclassOf: lambda p, s, o, bounding: self._evaluateSimpleGoal(p, s, o, bounding, self.reasoner.whatSuperclasses, self.reasoner.whatSubclasses),
-                      self.isInstanceOf: lambda p, s, o, bounding: self._evaluateSimpleGoal(p, s, o, bounding, self._ensureIndividual2Classes, self._ensureClass2Individuals)}
+        self.fnMap = {self.hasDisposition: lambda g, p, s, o, bounding: self._evaluateSimpleGoal(g, p, s, o, bounding, self.reasoner.whatDispositionsDoesObjectHave, self.reasoner.whatHasDisposition),
+                      self.hasPart: lambda g, p, s, o, bounding: self._evaluateSimpleGoal(g, p, s, o, bounding, self.reasoner.whatPartTypesDoesObjectHave, self.reasoner.whatHasPartType),
+                      self.hasConstituent: lambda g, p, s, o, bounding: self._evaluateSimpleGoal(g, p, s, o, bounding, self.reasoner.whatConstituentsDoesObjectHave, self.reasoner.whatHasConstituent),
+                      self.isSubclassOf: lambda g, p, s, o, bounding: self._evaluateSimpleGoal(g, p, s, o, bounding, self.reasoner.whatSuperclasses, self.reasoner.whatSubclasses),
+                      self.isInstanceOf: lambda g, p, s, o, bounding: self._evaluateSimpleGoal(g, p, s, o, bounding, self._ensureIndividual2Classes, self._ensureClass2Individuals)}
         self.classes = set()
         
-    def _evaluateSimpleGoal(self, p, s, o, bounding, fnOVar, fnSVar):
+    def _evaluateSimpleGoal(self, goal, p, s, o, bounding, fnOVar, fnSVar):
         if ((False, True) == bounding):
             todo = self._ensureIndividual2Classes(s)
             dispositions = set().union(*[fnOVar(str(c)) for c in todo])
             _ = [goal.push(Bindings({o: IRIAtom(self.reasoner.expandName(d))})) for d in dispositions]
         else:
             concepts = [self.reasoner.expandName(c) for c in fnSVar(str(o))]
-            instances = set.union(*[self._ensureClass2Individuals(c) for c in concepts])
+            instances = set().union(*[self._ensureClass2Individuals(c) for c in concepts])
             if ((True, False) == bounding):
+                logWarn("%s" % str(type(s)))
                 _ = [goal.push(Bindings({s: IRIAtom(self.reasoner.expandName(i))})) for i in instances]
             elif ((False, False) == bounding) and str(s) in instances:
                 goal.push(Bindings({}))
@@ -87,11 +88,12 @@ class SOMADFLReasoner(GoalReasoner):
 
     def evaluate(self, goal: Goal) -> bool:
         # Assume only simple goals for now.
-        literal = goal.literals()[0].predicate()
-        p = _iriOrVariable(literal.functor())
+        literal = goal.formula().literals()[0].predicate()
+        p = _iriOrVariable(self.reasoner, literal.functor())
+        logWarn("%s %s" % (repr(p), str(self.simpleGoals)))
         if p in self.simpleGoals:
-            s : Term = _iriOrVariable(literal.arguments()[0])
-            o : Term = _iriOrVariable(literal.arguments()[1])
+            s : Term = _iriOrVariable(self.reasoner, literal.arguments()[0])
+            o : Term = _iriOrVariable(self.reasoner, literal.arguments()[1])
             logDebug("Checking %s(%s, %s)" % (str(p), str(s), str(o)))
             bounding = (s.isVariable(), o.isVariable())
             args = [x for x in [s, o] if not x.isVariable()]
@@ -102,11 +104,11 @@ class SOMADFLReasoner(GoalReasoner):
                 o = x
             if (True, True) == bounding:
                 raise ValueError("Must specify at least one of the participants in the %s goal." % str(p))
-            self.fnMap[p](p, s, o, bounding)
+            self.fnMap[p](goal, p, s, o, bounding)
         else: # useMatch goal
-            task : Term = _iriOrVariable(literal.arguments()[0])
-            instrument : Term = _iriOrVariable(literal.arguments()[1])
-            patient : Term = _iriOrVariable(literal.arguments()[2])
+            task : Term = _iriOrVariable(self.reasoner, literal.arguments()[0])
+            instrument : Term = _iriOrVariable(self.reasoner, literal.arguments()[1])
+            patient : Term = _iriOrVariable(self.reasoner, literal.arguments()[2])
             # TODO: maybe implement questions of type "what could I do with these objects"
             if task.isVariable():
                 raise ValueError("Task must be specified for useMatch query")
